426c963ce5739fb4126f62b76ce5e04af19a5063
Christophe Massolin <christophe.massolin@gmail.com>
2018-02-18 11:47:23 +0100

    Add optional sources

---
+++ template.js
@@ -0,0 +1,9 @@
+/* eslint-disable */
+//@source=./skeleton/template.js
+//@source?=./environment/template.js
+//@source?=./bump/template.js
+//@source?=./git/template.js
+//@source?=./i18n/template.js
+//@source?=./aws/template.js
+//@source?=./template/template.js
+//@source?=./vue/template.js
