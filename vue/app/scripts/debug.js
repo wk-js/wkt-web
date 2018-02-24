a0f0dea6abdf2632f58db41a1cb99560c6f87150
Christophe Massolin <christophe.massolin@gmail.com>
2018-02-24 23:54:55 +0100

    Move vue boilerplate to starter-vue

--- vue/app/scripts/debug.js
+++
@@ -1,8 +0,0 @@
-import { createLogger } from './lib/utils/logger'
-
-/**
- * Debug
- */
-if (process.env.NODE_ENV === 'development') {
-  createLogger('Application', true)
-}
--- vue/app/scripts/lib/notification-center.js
+++
@@ -1,4 +0,0 @@
-'use strict'
-
-import EventEmitter from 'eventemitter3'
-export const NC = new EventEmitter()
