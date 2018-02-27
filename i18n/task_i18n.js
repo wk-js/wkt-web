02d66b3a1c4834f8b049893c266892c48d4b05c4
Christophe Massolin <christophe.massolin@makemepulse.com>
2018-02-27 15:02:51 +0100

    Update I18n task

--- i18n/task_i18n.js
+++ i18n/task_i18n.js
@@ -1,115 +1,115 @@
 #!/usr/bin/env node
-'use strict';
+'use strict'
 
-const GoogleSpreadsheet = require("google-spreadsheet");
-const FileUtils         = require( 'asset-pipeline/js/utils/fs' )
-const { relative }      = require( "path" )
+const GoogleSpreadsheet = require('google-spreadsheet')
+const FileUtils = require('asset-pipeline/js/utils/fs')
+const { relative } = require('path')
+const when = require('when')
+const lift = require('when/node').lift
 
+const excludedLocale = []
+const excludedWorksheet = [/^_/]
+const excludedCategory = []
+const excludedKey = []
 
-var langs         = []
+let locales = []
+let dstPath = null
+let sortedData = {}
 
-// ORDER MUST MATCH SPREADSHEET TABS ORDER
-var tabs = [
-  'global',
-  'menu',
-  'home',
-  'project',
-  'm-project',
-  'mining',
-  'stats',
-  'user-stats',
-  'faq'
-]
-var currentTabIdx = -1
-var sortedData    = {}
-var sheet
-var dst_path
-
-function _processTabs() {
-
-  currentTabIdx++
-
-  sheet.getRows( currentTabIdx + 1, function(err, row_data) {
-
-    var tab = tabs[currentTabIdx]
-
-    for ( var i = 0; i < row_data.length; i++ ) {
-
-      var category = row_data[i].category
-      if (category === '---' ) continue
-
-      var key = row_data[i].key
-
-      for (var j = 0; j < langs.length; j++) {
-
-        var lang = langs[j]
-
-        if( sortedData[lang][tab][category] === undefined ) sortedData[lang][tab][category] = {}
-
-        sortedData[lang][tab][category][key] = row_data[i][lang.toLowerCase()]
-
-      }
-
+function isExcluded (str, patterns) {
+  for (let i = 0, l = patterns.length; i < l; i++) {
+    const pattern = patterns[i]
+    if (str.match(pattern)) {
+      return true
     }
-
-    if ( currentTabIdx < tabs.length - 1 ) {
-      _processTabs()
-    }
-    else {
-      _writeToFile()
-    }
-
-  }.bind( this ))
-
+  }
+  return false
 }
 
-function _writeToFile() {
-  Object.keys( sortedData ).forEach(function(locale) {
-    const content = JSON.stringify( sortedData[locale], null, 2 )
-    FileUtils.writeFile( content, dst_path + '/' + locale + '.json' )
+function processSheet (data) {
+  const worksheets = data.worksheets
+  const p = []
+
+  for (let i = 0, l = worksheets.length; i < l; i++) {
+    const worksheet = worksheets[i]
+    const worksheetTitle = worksheet.title
+    if (isExcluded(worksheetTitle, excludedWorksheet)) {
+      continue
+    }
+
+    for (let j = 0, ll = locales.length; j < ll; j++) {
+      const locale = locales[j]
+      if (isExcluded(locale, excludedLocale)) {
+        continue
+      }
+
+      if (!sortedData[locale]) {
+        sortedData[locale] = {}
+        sortedData[locale].locale = locale
+      }
+
+      sortedData[locale][worksheetTitle] = {}
+
+      const getRows = lift(worksheet.getRows)
+      const promise = getRows()
+        .then(data => processRows(locale, worksheetTitle, data))
+      p.push(promise)
+    }
+  }
+
+  return when.all(p)
+}
+
+function processRows (locale, worksheetTitle, rowsData) {
+  for (let i = 0, l = rowsData.length; i < l; i++) {
+    const rowData = rowsData[i]
+    const category = rowData.category
+    const key = rowData.key
+
+    if (!key ||
+      isExcluded(category, excludedCategory) ||
+      isExcluded(key, excludedKey)
+    ) {
+      continue
+    }
+
+    if (!sortedData[locale][worksheetTitle][category]) {
+      sortedData[locale][worksheetTitle][category] = {}
+    }
+
+    sortedData[locale][worksheetTitle][category][key] = rowData[locale]
+  }
+}
+
+function writeToFile () {
+  Object.keys(sortedData).forEach(locale => {
+    const content = JSON.stringify(sortedData[locale], null, 2)
+    FileUtils.writeFile(content, dstPath + '/' + locale + '.json')
+    console.log(`File ${locale}.json written`)
   })
 }
 
+function executeLocales () {
+  const Application = require('../index')
+  const config = Application.config('i18n')
 
+  locales = config.locales
+  dstPath = Application.assets.root_path + '/config/locales'
+  dstPath = relative(process.cwd(), dstPath)
 
+  const sheet = new GoogleSpreadsheet(config.spreadSheetKey)
+  const getInfo = lift(sheet.getInfo)
 
-
-function executeLocales() {
-
-  const Application = require( '../index' )
-  const AppConfig   = Application.config('i18n')
-
-  const config  = { langs: AppConfig.locales }
-  const key     = AppConfig.spreadSheetKey
-  dst_path      = Application.assets.root_path + '/config/locales'
-  dst_path      = relative( process.cwd(), dst_path )
-
-  langs         = config.langs
-
-  sheet         = new GoogleSpreadsheet( key )
-
-  // initialize langs and tabs
-  for ( var i = 0; i < config.langs.length; i++ ) {
-    var lang = config.langs[i]
-    sortedData[lang] = {}
-    sortedData[lang].lang = lang
-
-    for ( var t = 0; t < tabs.length; t++ ) {
-      var tab = tabs[t]
-      sortedData[lang][tab] = {}
-    }
-
-  }
-
-  _processTabs()
-
+  return getInfo()
+    .then(processSheet)
+    .then(writeToFile)
 }
 
-task('default', { async: true }, function() {
-  const Application = require( '../index' )
+task('default', { async: true }, function () {
+  const Application = require('../index')
   Application
-  .make()
-  .then(executeLocales)
-  .then(this.complete)
-  .catch(this.fail)
+    .make()
+    .then(executeLocales)
+    .then(this.complete)
+    .catch(this.fail)
 })
